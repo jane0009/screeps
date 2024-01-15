f34adf12a931acc252301d7216a9eef28e970ab2
Jane Shimmer <jane@j4.pm>
2024-01-14 19:14:21 -0500

    nix moment

---
+++ shell.nix
@@ -0,0 +1,12 @@
+with import <nixpkgs> {};
+mkShell {
+  packages = [
+    libstdcxx5
+    fnm
+  ];
+  NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
+    stdenv.cc.cc
+    openssl
+  ];
+  NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
+}
